5cda493ded9e20107b8799fd7428a488cb02ae50
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2024-07-29 15:42:37 +0100

    step07

---
+++ step07/vae.py
@@ -0,0 +1,99 @@
+import torch
+from torchvision import datasets, transforms
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super().__init__()
+        self.linear = nn.Linear(input_dim, hidden_dim)
+        self.linear_mu = nn.Linear(hidden_dim, latent_dim)
+        self.linear_logvar = nn.Linear(hidden_dim, latent_dim)
+
+    def forward(self, x):
+        h = self.linear(x)
+        h = F.relu(h)
+        mu = self.linear_mu(h)
+        logvar = self.linear_logvar(h)
+        sigma = torch.exp(0.5 * logvar)
+        return mu, sigma
+    
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.linear_1 = nn.Linear(latent_dim, hidden_dim)
+        self.linear_2 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, z):
+        h = self.linear_1(z)
+        h = F.relu(h)
+        h = self.linear_2(h)
+        x_hat = F.sigmoid(h)
+        return x_hat
+    
+def reparametrize(mu, sigma):
+    eps = torch.randn_like(sigma)
+    return mu + eps * sigma
+
+class VAE(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super().__init__()
+        self.encoder = Encoder(input_dim=input_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
+        self.decoder = Decoder(latent_dim=latent_dim, hidden_dim=hidden_dim, output_dim=input_dim)
+
+    def get_loss(self, x):
+        mu, sigma = self.encoder(x)
+        z = reparametrize(mu, sigma)
+        x_hat = self.decoder(z)
+        batch_size = len(x)
+        L1 = F.mse_loss(x_hat, x, reduction='sum')
+        L2 = - torch.sum(1 + torch.log(sigma**2) - mu**2 - sigma**2)
+        return (L1+L2) / batch_size
+
+input_dim = 784
+hidden_dim = 200 # NN
+latent_dim = 20
+epochs = 30
+learning_rate = 3e-4
+batch_size = 32
+
+transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Lambda(torch.flatten) # flatten
+            ])
+dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+model = VAE(input_dim, hidden_dim, latent_dim)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+losses = []
+
+for epoch in range(epochs):
+    loss_sum = 0
+    cnt = 0
+    for x, label in dataloader:
+        optimizer.zero_grad()
+        loss = model.get_loss(x)
+        loss.backward()
+        optimizer.step()
+        loss_sum += loss.item()
+        cnt += 1
+    loss_avg = loss_sum / cnt
+    losses.append(loss_avg)
+    print(loss_avg)
+
+with torch.no_grad():
+    sample_size = 64
+    z = torch.randn(sample_size, latent_dim)
+    x = model.decoder(z)
+    generated_images = x.view(sample_size, 1, 28, 28)
+
+grid_img = torchvision.utils.make_grid(
+    generated_images, nrow=8, padding=2, normalize=True
+)
+plt.imshow(grid_img.permute(1, 2, 0))
+plt.axis('off')
+plt.show()
